662ee5e394aba4077d698ba611c0f8490278aba3
Tayfur Erkenci <terkenci@gmail.com>
2020-11-11 01:26:13 +0300

    component parts 2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import { DISHES } from './shared/dishes';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-    <Navbar dark color="primary">
-      <div className="container">
-        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-      </div>
-    </Navbar>
-  </div>
-  );
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render(){
+    return (
+      <div className="App">
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
+    </div>
+    );
+  }
+  
 }
 
 export default App;
