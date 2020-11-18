af6c8eaf91c06e22632dcf1ac512d1d8ff8a130b
Tayfur Erkenci <terkenci@gmail.com>
2020-11-18 19:44:30 +0300

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle } from 'reactstrap';
-
+import React from 'react';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
     function RenderDish({dish}) {
         if (dish != null)
             return(
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                      <CardTitle>{dish.name}</CardTitle>
-                      <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
+                    <Card>
+                        <CardImg top src={dish.image} alt={dish.name} />
+                        <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
             );
         else
             return(
@@ -20,36 +20,28 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,
             );
     }
 
-    function RenderComments({dish},{comments}){
+    function RenderComments({comments}){
         
         const options = { year: 'numeric', month: 'long', day: 'numeric' };{/*weekday: 'long', */}
-        if (dish != null){
-            const com = dish.comments.map((comment) =>
+        if (comments != null){
+            const com = comments.map((comment) =>
             {
-                
                 return (
-                    <div>
+                    <div className="col-12 col-md-5 m-1">
+                        <h4>Comments</h4>
                         <ul className="list-unstyled">
-                            <li>
-                                <p>
-                                    {comment.comment}
-                                </p>
-                                <p>
-                                    
-                                    -- {comment.author}, {new Date(comment.date).toLocaleDateString("en-US", options)}
-                                    
-                                </p>
-                            </li>
+                            {comments.map((comment) => {
+                                return (
+                                    <li key={comment.id}>
+                                        <p>{comment.comment}</p>
+                                        <p>-- {comment.author}, {new Date(comment.date).toLocaleDateString("en-US", options)}</p>
+                                    </li>
+                                );
+                            })}
                         </ul>
                     </div>
                 );
             })
-            return(
-                <div>
-                    <h4>Comments</h4>
-                    {com}
-                </div>
-            );
         }
         else
             return(
@@ -59,18 +51,33 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,
   
 
     const DishDetail = (props) => {
-        return (
+        if(props.dish != null)
+            return (
             <div className="container">
                 <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                    </div>
+                <div className="row">
                     <div className="col-12 col-md-5 m-1">
-                        <RenderDish dish={props.dish}/>
+                        <RenderDish dish={props.dish} />
                     </div>
                     <div className="col-12 col-md-5 m-1">
-                        <RenderComments dish={props.dish} />
+                        <RenderComments comments={props.comments} />
                     </div>
                 </div>
-            </div>
+                </div>
         );
+        else
+                return(
+                    <div></div>
+                );
     }
         
 
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -25,17 +25,21 @@ class Main extends Component {
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-  }
+  
 
   render() {
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
     const HomePage = () => {
       return(
         <Home 
-        dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-        promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-        leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+            dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+            promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+            leader={this.state.leaders.filter((leader) => leader.featured)[0]}
         />
       );
     }
@@ -46,6 +50,7 @@ class Main extends Component {
         <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route path='/menu/:dishId' component={DishWithId} />
               <Route exact path='/contactus' component={Contact} />
               <Redirect to="/home" />
           </Switch>
