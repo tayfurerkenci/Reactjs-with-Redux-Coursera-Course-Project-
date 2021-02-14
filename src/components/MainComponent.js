c4cfaf8f6cbbb5c8606634c355acda30b4179470
Tayfur Erkenci <terkenci@gmail.com>
2021-02-15 00:49:23 +0300

    React Redux Forms Revisited

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux';
 import { addComment, fetchDishes } from '../redux/ActionCreators';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { actions } from 'react-redux-form';
 
 
 const mapStateToProps = state => {
@@ -23,7 +24,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => ({
   fetchDishes: () => { dispatch(fetchDishes())},
-  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
+  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
+  resetFeedbackForm: () => { dispatch(actions.reset('feedback'))}
 
 });
 
@@ -63,7 +65,7 @@ class Main extends Component {
               <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} /> }/>
               <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
-              <Route exact path='/contactus' component={Contact} />
+              <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
               <Redirect to="/home" />
           </Switch>
         </div>
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,22 +1,26 @@
-import {createStore, combineReducers, applyMiddleware } from 'redux';
-import { Reducer, initialState } from './reducer'
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import { Dishes } from './dishes';
-import { Comments } from './comments';
-import { Promotions } from './promotions';
-import { Leaders } from './leaders';
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createForms } from "react-redux-form";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { InitialFeedback } from "./forms";
+import { Dishes } from "./dishes";
+import { Comments } from "./comments";
+import { Promotions } from "./promotions";
+import { Leaders } from "./leaders";
 
 export const ConfigureStore = () => {
-    const store = createStore(
-        combineReducers({
-        dishes: Dishes,
-        comments: Comments,
-        promotions: Promotions,
-        leaders: Leaders
+  const store = createStore(
+    combineReducers({
+      dishes: Dishes,
+      comments: Comments,
+      promotions: Promotions,
+      leaders: Leaders,
+      ...createForms({
+        feedback: InitialFeedback
+      })
     }),
     applyMiddleware(thunk, logger)
+  );
 
-    );
-    return store;
-}
+  return store;
+};
