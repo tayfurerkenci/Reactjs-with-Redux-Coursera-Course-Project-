93686f15e490778d9e5aa37ae5797cb80eaf7adb
Tayfur Erkenci <terkenci@gmail.com>
2021-02-17 01:18:47 +0300

    my final work

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,5 +1,4 @@
 import * as ActionTypes from './ActionTypes';
-import { DISHES } from '../shared/dishes';
 import { baseUrl } from '../shared/baseUrl';
 
 export const addComment = (comment) => ({
@@ -132,16 +131,93 @@ export const fetchPromos = () => (dispatch) => {
     .catch(error => dispatch(promosFailed(error.message)));
 }
 
+
+
 export const promosLoading = () => ({
-    type: ActionTypes.PROMOS_LOADING
+  type: ActionTypes.PROMOS_LOADING
 });
 
 export const promosFailed = (errmess) => ({
-    type: ActionTypes.PROMOS_FAILED,
-    payload: errmess
+  type: ActionTypes.PROMOS_FAILED,
+  payload: errmess
 });
 
 export const addPromos = (promos) => ({
-    type: ActionTypes.ADD_PROMOS,
-    payload: promos
+  type: ActionTypes.ADD_PROMOS,
+  payload: promos
 });
+
+export const leadersLoading = () => ({
+  type: ActionTypes.LEADERS_LOADING
+});
+
+export const leadersFailed = (errmess) => ({
+  type: ActionTypes.LEADERS_FAILED,
+  payload: errmess
+});
+
+export const addLeaders = (leaders) => ({
+  type: ActionTypes.ADD_LEADERS,
+  payload: leaders
+});
+
+export const fetchLeaders = () => (dispatch) => {
+  
+dispatch(leadersLoading());
+
+return fetch(baseUrl + 'leaders')
+.then(response => {
+    if (response.ok) {
+      return response;
+    } else {
+      var error = new Error('Error ' + response.status + ': ' + response.statusText);
+      error.response = response;
+      throw error;
+    }
+  },
+  error => {
+        var errmess = new Error(error.message);
+        throw errmess;
+  })
+.then(response => response.json())
+.then(leaders => dispatch(addLeaders(leaders)))
+.catch(error => dispatch(leadersFailed(error.message)));
+}
+
+
+export const postFeedback = (firstname, lastname, telnum, email, agree, contactType, message) => (dispatch) => {
+
+  const newFeedback = {
+    firstname: firstname,
+    lastname: lastname,
+    telnum: telnum,
+    email: email,
+    agree: agree,
+    contactType: contactType,
+    message: message
+  };
+  
+  return fetch(baseUrl + 'feedback', {
+      method: "POST",
+      body: JSON.stringify(newFeedback),
+      headers: {
+        "Content-Type": "application/json"
+      },
+      credentials: "same-origin"
+  })
+  .then(response => {
+      if (response.ok) {
+        return response;
+      } else {
+        var error = new Error('Error ' + response.status + ': ' + response.statusText);
+        error.response = response;
+        throw error;
+      }
+    },
+    error => {
+          throw error;
+    })
+  .then(response => response.json())
+  .then(response => alert("Thank you for your feedback!" + JSON.stringify(response)))
+  .catch(error =>  { console.log('post feedbacks', error.message); alert('Your comment could not be posted\nError: '+error.message); });
+};
